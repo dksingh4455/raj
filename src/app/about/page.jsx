33d4b7dd6b20a1f1b64a6cb668752d055b83e21e
dksingh4455 <gitinsuri982@gmail.com>
2024-12-30 18:56:12 +0530

    fix

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Navbar from "../../components/navbar";
-import Cardimg from "../../components/cardimg";
-import Counter from "../../components/counter";
-import Review from "../../components/review";
+import Navbar from "@/components/navbar";
+import Cardimg from "@/components/cardimg";
+import Counter from "@/components/counter";
+import Review from "@/components/review";
 
 
 const Aboutus = () => {
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "@/components/navbar";
-import Choose from "../../components/choose";
-import Cardsnew from "../../components/cardsnew";
+import Choose from "@/components/choose"
+import Cardsnew from "@/components/cardsnew"
+
 const Contact = () => {
   return (
     <>
