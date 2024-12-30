6566a0e62488e55ef0fd6ee7e5232942eafef5de
dksingh4455 <gitinsuri982@gmail.com>
2024-12-30 18:50:51 +0530

    fix

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../../components/navbar";
+import Navbar from "@/components/navbar";
 import Choose from "../../components/choose";
 import Cardsnew from "../../components/cardsnew";
 const Contact = () => {
